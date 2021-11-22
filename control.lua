63749b556c370d75982e6849f4063544a66f658b
Dusk <dusk@woofle.net>
2021-11-22 15:32:26 -0800

    1.1.0
    Remove cinematic zoom; simplify logic accordingly

--- control.lua
+++ control.lua
@@ -1,59 +1,25 @@
-speedmap = {
-    zero    = 0,
-    vfast   = 6,
-    fast    = 15,
-    medium  = 30,
-    slow    = 60,
-}
+function vzoom_update(event)
+    local player = game.get_player(event.player_index)
+    if player.controller_type ~= defines.controllers.character then
+        return
+    end
 
-script.on_event(
-    defines.events.on_player_driving_changed_state,
-    function (event)
-        local player = game.get_player(event.player_index)
-        if player.controller_type ~= defines.controllers.character then
-            return
-        end
+    local cfg = settings.get_player_settings(event.player_index)
 
-        local cfg = settings.get_player_settings(event.player_index)
-
-        local vkind = "car"
-        if event.entity then
-            if event.entity.type == "car" then
-                vkind = "car"
-            elseif event.entity.type == "locomotive" or string.match(event.entity.type, "-wagon") then
-                vkind = "train"
-            elseif event.entity.type == "spider-vehicle" then
-                vkind = "spider"
-            end
-        end
-
-        local oldlevel, newlevel
-        if player.driving then
-            oldlevel = cfg["vzoom-lv-walk"].value
-            newlevel = cfg["vzoom-lv-" .. vkind].value
-        else
-            newlevel = cfg["vzoom-lv-walk"].value
-            oldlevel = cfg["vzoom-lv-" .. vkind].value
-        end
-
-        if oldlevel and newlevel and oldlevel ~= newlevel then
-            local speed = speedmap[cfg["vzoom-speed"].value]
-            if speed > 0 then 
-                player.set_controller({
-                    type = defines.controllers.cutscene,
-                    character = player.character,
-                    waypoints = {
-                        {
-                            target = player.character,
-                            transition_time = speed,
-                            time_to_wait = 0,
-                            zoom = newlevel,
-                        },
-                    },
-                })
-            else
-                player.zoom = newlevel
-            end
+    local zoom = cfg["vzoom-lv-walk"].value
+    if player.vehicle then
+        if player.vehicle.type == "car" then
+            zoom = cfg["vzoom-lv-car"].value
+        elseif player.vehicle.type == "locomotive" or string.match(player.vehicle.type, "-wagon") then
+            zoom = cfg["vzoom-lv-train"].value
+        elseif player.vehicle.type == "spider-vehicle" then
+            zoom = cfg["vzoom-lv-spider"].value
         end
     end
-)
+
+    player.zoom = zoom * player.display_scale
+end
+
+script.on_event(defines.events.on_player_driving_changed_state, vzoom_update)
+script.on_event(defines.events.on_player_display_scale_changed, vzoom_update)
+script.on_event(defines.events.on_runtime_mod_setting_changed, vzoom_update)
--- settings.lua
+++ settings.lua
@@ -1,27 +1,12 @@
 data:extend({
     {
-        name = "vzoom-speed",
-        type = "string-setting",
-        setting_type = "runtime-per-user",
-        default_value = "medium",
-        allowed_values = {
-            "zero",
-            "vfast",
-            "fast",
-            "medium",
-            "slow",
-        },
-        order = 0,
-    },
-
-    {
         name = "vzoom-lv-walk",
         type = "double-setting",
         setting_type = "runtime-per-user",
         default_value = 1.0,
-        minimum_value = 0.2,
+        minimum_value = 0.1,
         maximum_value = 4.0,
-        order = 1,
+        order = 0,
     },
 
     {
@@ -31,7 +16,7 @@ data:extend({
         default_value = 0.5,
         minimum_value = 0.1,
         maximum_value = 4.0,
-        order = 2,
+        order = 1,
     },
 
     {
@@ -41,7 +26,7 @@ data:extend({
         default_value = 0.5,
         minimum_value = 0.1,
         maximum_value = 4.0,
-        order = 3,
+        order = 2,
     },
 
     {
@@ -51,6 +36,6 @@ data:extend({
         default_value = 1.0,
         minimum_value = 0.1,
         maximum_value = 4.0,
-        order = 4,
+        order = 3,
     },
 })
